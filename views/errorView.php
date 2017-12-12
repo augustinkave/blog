779540628ee81022a9cfba29c00e69c6b2506ef2
kavera <augustin.kavera@outlook.fr>
2017-12-12 14:04:16 +0100

    Ajout de errorView

---
+++ views/errorView.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+
+      <meta charset="utf-8">
+      <title>Page d'erreur</title>
+      <link rel="stylesheet" href="">
+
+    </head>
+
+    <body>
+        <h1><?= $errorMessage; ?></h1>
+
+        <p><a href="../../">Retour à la liste des billets</a></p>
+    </body>
+</html>
